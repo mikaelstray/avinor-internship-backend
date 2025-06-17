eea201e830c0d060642a02dc27d70676231d3cb5
Mikael Stray Frøyshov (Client Local Admin) <CAMFY1@lv.no>
2025-06-17 15:37:03 +0200

    Initial Spring Boot setup

---
+++ gate-occupancy/src/main/java/no/avinor/gate_occupancy/GateOccupancyApplication.java
@@ -0,0 +1,13 @@
+package no.avinor.gate_occupancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateOccupancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateOccupancyApplication.class, args);
+	}
+
+}
---
+++ gate-occupancy/src/test/java/no/avinor/gate_occupancy/GateOccupancyApplicationTests.java
@@ -0,0 +1,13 @@
+package no.avinor.gate_occupancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateOccupancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
