632fae735181cf8706d960f206f3f2fe52c399ce
mikael <mikael.stray.froyshov@avinor.no>
2025-06-26 10:16:44 +0200

    feat: update model and dto files

--- gate-occupancy/src/main/java/no/avinor/gate_occupancy/controller/AreaController.java
+++ gate-occupancy/src/main/java/no/avinor/gate_occupancy/controller/AreaController.java
@@ -1,37 +1,22 @@
 package no.avinor.gate_occupancy.controller;
 
-import no.avinor.gate_occupancy.dto.AreaDTO;
+import lombok.RequiredArgsConstructor;
 import no.avinor.gate_occupancy.service.AreaService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * REST controller that handles HTTP requests related to airport areas.
- * Exposes endpoints for retrieving real-time crowdiness status of airport zones (e.g., gates).
- */
 @RestController
-@RequestMapping("/api/areas") // Base path for all area-related endpoints
+@RequestMapping("/api/areas")
+@RequiredArgsConstructor
+@Validated
+
 public class AreaController {
 
     private final AreaService areaService;
+    private static final Logger logger = LogManager.getLogger(AreaController.class);
 
-    /**
-     * Constructor injection for the AreaService dependency.
-     * Spring automatically injects the appropriate implementation.
-     */
-    public AreaController(AreaService areaService) {
-        this.areaService = areaService;
-    }
+    @
 
-    /**
-     * GET endpoint that returns crowdiness status for a specific area by ID.
-     *
-     * Example: GET /api/areas/101/status
-     *
-     * @param id Area ID to look up
-     * @return DTO containing area and status info
-     */
-    @GetMapping("/{id}/status")
-    public AreaDTO getAreaStatus(@PathVariable Long id) {
-        return areaService.getAreaStatus(id);
-    }
 }
---
+++ gate-occupancy/src/main/java/no/avinor/gate_occupancy/model/entities/OccupancyLiveStatus.java
@@ -0,0 +1,34 @@
+package no.avinor.gate_occupancy.model.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.descriptor.jdbc.TinyIntAsSmallIntJdbcType;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Accessors(chain = true)
+
+@Table(name = "occupancy_live_status")
+public class OccupancyLiveStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Integer newPax;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modify_date")
+    private LocalDateTime updatedTime;
+}
