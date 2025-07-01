b195f32857d745c6c92828b3756f516faba32d16
mikael <mikael.stray.froyshov@avinor.no>
2025-07-01 13:55:29 +0200

    feat(websocket): add websocket setup

---
+++ gate-occupancy/src/main/java/no/avinor/gate_occupancy/service/PaxNotifierService.java
@@ -0,0 +1,22 @@
+package no.avinor.gate_occupancy.service;
+
+import lombok.RequiredArgsConstructor;
+import no.avinor.gate_occupancy.model.dto.occupancyStatus.LiveStatusResponse;
+import no.avinor.gate_occupancy.model.entities.LocationLiveOccupancy;
+import no.avinor.gate_occupancy.model.mappers.LiveOccupancyMapper;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaxNotifierService {
+    private final SimpMessagingTemplate messagingTemplate;
+    private final LiveOccupancyMapper mapper;
+
+    public void notifyPaxUpdate(LocationLiveOccupancy liveOccupancy) {
+        String dest = "/topic/pax-updates";
+        LiveStatusResponse response = mapper.toDto(liveOccupancy);
+
+        if (response != null) messagingTemplate.convertAndSend(dest, response);
+    }
+}
