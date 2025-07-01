64f6ff1e1fe061ec814a181cfcff7dafd6bdbe47
mikael <mikael.stray.froyshov@avinor.no>
2025-07-01 13:51:47 +0200

    feat(erro handling): implement global exception handler

---
+++ gate-occupancy/src/main/java/no/avinor/gate_occupancy/exception/CustomErrorMessage.java
@@ -0,0 +1,29 @@
+package no.avinor.gate_occupancy.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CustomErrorMessage {
+
+  // --- Location Errors ---
+  LOCATION_ALREADY_EXISTS    (409, "Location already exists."),
+  LOCATION_NOT_FOUND             (404, "Location not found."),
+
+  // --- ... Errors ---
+  INVALID___DATA          (400, "... data is invalid."),
+
+  // ---  errors ---
+  LOCATION_NOT_IN_ZONE(404, "Location is not in zone"),
+
+  // --- Authorization ---
+  UNAUTHORIZED_OPERATION     (403, "You are not authorized to perform this operation."),
+
+  // --- Generic ---
+  INTERNAL_SERVER_ERROR      (500, "An unexpected internal server error occurred.");
+
+
+  private final int status;
+  private final String message;
+}
