419c63bbeaff2b6d10cf59e6d654ae2b3d7402c0
mikael <mikael.stray.froyshov@avinor.no>
2025-07-03 15:08:51 +0200

    refactor(pr): fix pr comments v3

--- gate-occupancy/src/main/java/no/avinor/gate_occupancy/model/entities/Airport.java
+++ gate-occupancy/src/main/java/no/avinor/gate_occupancy/model/entities/Airport.java
@@ -1,8 +1,18 @@
 package no.avinor.gate_occupancy.model.entities;
 
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.util.ArrayList;
--- gate-occupancy/src/main/java/no/avinor/gate_occupancy/model/entities/Zone.java
+++ gate-occupancy/src/main/java/no/avinor/gate_occupancy/model/entities/Zone.java
@@ -1,13 +1,21 @@
 package no.avinor.gate_occupancy.model.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
-import java.util.HashSet;
 import java.util.Set;
 
 @Entity
